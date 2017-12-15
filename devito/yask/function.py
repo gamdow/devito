57a2bb04aea46d1ad727484c14a4a9e11806ff3e
Fabio Luporini <f.luporini12@imperial.ac.uk>
2017-12-15 15:59:54 +0100

    domain-switch, core: Implement data_{domain,with_halo,padding}

--- devito/yask/function.py
+++ devito/yask/function.py
@@ -4,6 +4,7 @@ import numpy as np
 
 import devito.types as types
 import devito.function as function
+from devito.logger import yask as log
 from devito.tools import numpy_to_ctypes
 
 from devito.yask.data import Data, DataScalar
@@ -38,6 +39,8 @@ class Function(function.Function):
         """Allocate memory in terms of YASK grids."""
         def wrapper(self):
             if self._data is None:
+                log("Allocating memory for %s (%s)" % (self.name, self.shape))
+
                 # Fetch the appropriate context
                 context = contexts.fetch(self.grid, self.dtype)
 
@@ -107,11 +110,6 @@ class Function(function.Function):
     def data_with_halo(self):
         return Data(self._data.grid, self.shape_with_halo, self.indices, self.dtype,
                     offset=self._offset_halo)
-
-    @cached_property
-    @_allocate_memory
-    def data_allocated(self):
-        return self._data
 
     def initialize(self):
         raise NotImplementedError
